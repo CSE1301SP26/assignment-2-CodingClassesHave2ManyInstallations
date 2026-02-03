0927be3a1b0b1ff1dce876537542ff8f15808657
CodingClassesHave2ManyInstallations <bergman.z@wustl.edu>
2026-02-02 20:33:30 -0600

    Assignment 2 Completed

--- src/assignment2/Nim.java
+++ src/assignment2/Nim.java
@@ -8,12 +8,19 @@ public class Nim {
     int turnCounter = 0;
     int computerMove = 0;
     int winCounter = 0;
+    int x = 0;
     while (numSticks>0) {
         if (turnCounter==0) {
-            for (int i =0; i==1;i++) {
+            for (int i =0; i<1;i++) {
                 System.out.println("There are " + numSticks + " sticks left.");
                 System.out.println("It's your turn! How many sticks will you remove? (1 or 2)");
-                numSticks = numSticks - scan.nextInt();
+                while (x==0) {
+                    x = scan.nextInt();
+                    if ((x!=1)&&(x!=2)) {
+                        x=0;
+                    }
+                }numSticks = numSticks - x;
+                x=0;
                 if (numSticks>0){
                     turnCounter++;
                 } if (numSticks<=0){
@@ -22,7 +29,7 @@ public class Nim {
                 }
             }
         } if (turnCounter==1) {
-            for (int i =0; i==1;i++) {
+            for (int i =0; i<1;i++) {
                 System.out.println("There are " + numSticks + " sticks left.");
                 double computerChance = Math.random();
                 if (computerChance<.5) {
@@ -44,6 +51,6 @@ public class Nim {
         System.out.println("Congrats! You win!");
     } if (winCounter == 2) {
         System.out.println("Oh no! The computer wins!");
-    }
+    }scan.close();
     }
 }
