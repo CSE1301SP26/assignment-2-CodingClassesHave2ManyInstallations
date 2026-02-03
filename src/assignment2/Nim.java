14348957d6b34a7222872ae035b17958b3851be0
CodingClassesHave2ManyInstallations <bergman.z@wustl.edu>
2026-02-02 20:12:25 -0600

    CP1

---
+++ src/assignment2/Nim.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+public class Nim {
+    public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    System.out.print("Enter the initial number of sticks: ");
+    int initSticks = scan.nextInt();
+    int numSticks = initSticks;
+    int turnCounter = 0;
+    int computerMove = 0;
+    int winCounter = 0;
+    while (numSticks>0) {
+        if (turnCounter==0) {
+            for (int i =0; i==1;i++) {
+                System.out.println("There are " + numSticks + " sticks left.");
+                System.out.println("It's your turn! How many sticks will you remove? (1 or 2)");
+                numSticks = numSticks - scan.nextInt();
+                if (numSticks>0){
+                    turnCounter++;
+                } if (numSticks<=0){
+                    turnCounter=2;
+                    winCounter=1;
+                }
+            }
+        } if (turnCounter==1) {
+            for (int i =0; i==1;i++) {
+                System.out.println("There are " + numSticks + " sticks left.");
+                double computerChance = Math.random();
+                if (computerChance<.5) {
+                    computerMove = 1;
+                } if (computerChance>=.5) {
+                    computerMove = 2;
+                } numSticks = numSticks - computerMove;
+                System.out.println("The computer removed " + computerMove + " stick(s). There are " + numSticks + " sticks left.");
+                if (numSticks>0){
+                    turnCounter--;
+                } if (numSticks<=0){
+                    turnCounter=2;
+                    winCounter=2;
+                }
+            }
+        } 
+    } 
+    if (winCounter == 1) {
+        System.out.println("Congrats! You win!");
+    } if (winCounter == 2) {
+        System.out.println("Oh no! The computer wins!");
+    }
+    }
+}
